500378e88fcea9189198b1ad291baed92ba2f98e
Transmissions <greenfeatherhelp@gmail.com>
2019-12-19 18:06:52 -0800

    Site config

---
+++ website/siteConfig.js
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// See https://docusaurus.io/docs/site-config for all the possible
+// site configuration options.
+
+const siteConfig = {
+  title: "YOUR_APP_TITLE", // Title for your website.
+  tagline: "YOUR_APP_TAGLINE",
+  url: "https://YOUR_ORGANIZATION_NAME.github.io", // Your website URL
+  baseUrl: "/", // Base URL for your project */
+  // For github.io type URLs, you would set the url and baseUrl like:
+  //   url: 'https://facebook.github.io',
+  //   baseUrl: '/test-site/',
+
+  // Used for publishing and more
+  projectName: "YOUR_ORGANIZATION_NAME.github.io",
+  organizationName: "YOUR_ORGANIZATION_NAME",
+  // For top-level user or org sites, the organization is still the same.
+  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
+  //   organizationName: 'JoelMarcey'
+
+  // For no header links in the top nav bar -> headerLinks: [],
+  headerLinks: [
+    { doc: "getting-started", label: "Docs" },
+    { page: "help", label: "Help" },
+    { blog: true, label: "Blog" }
+  ],
+
+  /* path to images for header/footer */
+  headerIcon: "img/logo192.png",
+  footerIcon: "img/logo192.png",
+  favicon: "img/favicon.ico",
+
+  /* Colors for website */
+  colors: {
+    primaryColor: "#629599",
+    secondaryColor: "#44686b"
+  },
+
+  /* Custom fonts for website */
+  /*
+  fonts: {
+    myFont: [
+      "Times New Roman",
+      "Serif"
+    ],
+    myOtherFont: [
+      "-apple-system",
+      "system-ui"
+    ]
+  },
+  */
+
+  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
+  copyright: `Copyright © ${new Date().getFullYear()} YOUR_ORGANIZATION_NAME`,
+
+  highlight: {
+    // Highlight.js theme to use for syntax highlighting in code blocks.
+    theme: "default"
+  },
+
+  // Add custom scripts here that would be placed in <script> tags.
+  scripts: ["https://buttons.github.io/buttons.js"],
+
+  // On page navigation for the current documentation page.
+  onPageNav: "separate",
+  // No .html extensions for paths.
+  cleanUrl: true,
+
+  // Open Graph and Twitter card images.
+  ogImage: "img/undraw_online.svg",
+  twitterImage: "img/undraw_tweetstorm.svg",
+
+  // For sites with a sizable amount of content, set collapsible to true.
+  // Expand/collapse the links and subcategories under categories.
+  // docsSideNavCollapsible: true,
+
+  // Show documentation's last contributor's name.
+  // enableUpdateBy: true,
+
+  // Show documentation's last update time.
+  // enableUpdateTime: true,
+
+  // You may provide arbitrary config keys to be used as needed by your
+  // template. For example, if you need your repo's URL...
+  repoUrl: "https://github.com/YOUR_ORGANIZATION_NAME/YOUR_GITHUB_PROJECT"
+};
+
+module.exports = siteConfig;
